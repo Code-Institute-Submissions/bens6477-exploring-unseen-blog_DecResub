e4df6b37e1cd27090382b72fd14ee06426615fe1
Ben Smith <bens6477@gmail.com>
2022-02-24 16:28:28 +0000

    Add filters and prepopulation to Article model in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Article, Country
+from .models import Article, Country, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('approved', 'created_date')
+    list_display = ('title', 'country', 'created_date', 'updated_date', 'approved')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
 
 admin.site.register(Country)
