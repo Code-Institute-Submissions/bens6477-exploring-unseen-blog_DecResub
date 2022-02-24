cb2a6e2d07a989575a5acfe23fc09cc98b86b5ab
Ben Smith <bens6477@gmail.com>
2022-02-24 15:39:53 +0000

    Create superuser, add Article and Country models to admin workspace and create test article

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article, Country
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+admin.site.register(Country)
