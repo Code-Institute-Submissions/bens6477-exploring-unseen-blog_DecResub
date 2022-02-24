657b1e71ed57318a6fbee65914d1b7c160e5714b
Ben Smith <bens6477@gmail.com>
2022-02-24 18:35:51 +0000

    Create view, placeholder template and url for article list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Article
 
-# Create your views here.
+
+class ArticleList(generic.ListView):
+    model = Article
+    queryset = Article.objects.filter(approved=True).order_by('-created_date')
+    template_name = 'index.html'
+    paginate_by = 8
