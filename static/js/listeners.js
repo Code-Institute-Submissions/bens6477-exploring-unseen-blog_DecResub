a558df49da294675662c1fd76897a0fa9e909281
Ben Smith <bens6477@gmail.com>
2022-03-03 17:24:53 +0000

    Fix switch statement cases and update hero image background attributes

--- static/js/listeners.js
+++ static/js/listeners.js
@@ -5,16 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     if (hero){
         let heroPageAttribute = hero[0].getAttribute("data-hero-page");
         switch (heroPageAttribute){
-            case heroPageAttribute = "home":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2356045/pexels-photo-2356045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+            case (heroPageAttribute = "home"):
+                hero[0].style.background = "url('https://images.pexels.com/photos/2356045/pexels-photo-2356045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat bottom/cover fixed";
+                break;
             case heroPageAttribute = "article":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+                hero[0].style.background = "url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat bottom/cover fixed";
+                break;
             case heroPageAttribute = "login":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+                hero[0].style.background = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat bottom/cover fixed";
+                break;
             case heroPageAttribute = "signup":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+                hero[0].style.background = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat bottom/cover fixed";
+                break;
             case heroPageAttribute = "logout":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+                hero[0].style.background = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat bottom/cover fixed";
+                break;
         }
     }
 })
