193d4758b82b41fdad42f8b225ccf7c2125086b9
Ben Smith <bens6477@gmail.com>
2022-03-03 09:02:30 +0000

    Add view for article detail page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -8,3 +8,28 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(approved=True).order_by('-created_date')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class ArticleDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(approved=True)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by('created_on')
+        upvoted = False
+        if article.upvotes.filter(id=self.request.user.id).exists():
+            upvoted = True
+        downvoted = False
+        if article.downvotes.filter(id=self.request.user.id).exists():
+            downvoted = True
+        return render(
+            request,
+            "article_detail.html",
+            {
+                "article": article,
+                "comments": comments,
+                "upvoted": upvoted,
+                "downvoted": downvoted,
+            }   
+        )
+
+
