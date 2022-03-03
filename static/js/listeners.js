6ea52267e17dbfd89ce8ee8de630e3d08798678d
Ben Smith <bens6477@gmail.com>
2022-03-03 17:01:53 +0000

    Centralise jumbotrons on all account pages and change navbar from fixed to sticky

--- static/js/listeners.js
+++ static/js/listeners.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         let heroPageAttribute = hero[0].getAttribute("data-hero-page");
         switch (heroPageAttribute){
             case heroPageAttribute = "home":
-                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+                hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2356045/pexels-photo-2356045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
             case heroPageAttribute = "article":
                 hero[0].style.backgroundImage = "url('https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
             case heroPageAttribute = "login":
