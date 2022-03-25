632021c3c0051dbe2ecf5f7f26b3a2b122ff2003
Ben Smith <bens6477@gmail.com>
2022-03-25 14:50:08 +0000

    Connect article_downvote url to downvote button

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('upvote/<slug:slug>/', views.ArticleUpvote.as_view(), name='article_upvote'),
+    path('downvote/<slug:slug>/', views.ArticleDownvote.as_view(), name="article_downvote")
 ]
