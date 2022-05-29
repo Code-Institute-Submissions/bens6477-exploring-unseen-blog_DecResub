e025263eb8ac2146fbf4e9e10830d2f9213052b9
Ben Smith <bens6477@gmail.com>
2022-05-29 21:43:10 +0000

    Test views return correct templates for corresponding path

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -9,11 +9,11 @@ class TestCommentForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('body', form.errors.keys())
         self.assertEqual(form.errors['body'][0], 'This field is required.')
-    
+
     def test_form_is_valid_with_populated_body_field(self):
         form = CommentForm({'body': 'Test todo Item'})
         self.assertTrue(form.is_valid())
-    
+
     def test_fields_are_explicit_in_form_metaclass(self):
         form = CommentForm()
         self.assertEqual(form.Meta.fields, ('body',))
--- blog/test_views.py
+++ blog/test_views.py
@@ -1,7 +1,62 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Article, Country
 
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self): # self = TestDjango
-        self.assertEqual(1, 1)
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    def test_get_country_articles_page(self):
+        user = User.objects.create(username='fake')
+        country = Country.objects.create(
+            country_name='Fake',
+            approve_country=True)
+        article = Article.objects.create(
+            title='Third Test Article',
+            slug='third-test-article',
+            country_id=1, author_id=1,
+            attraction='Third Test Article',
+            summary='Summary',
+            content='Content',
+            approved=True)
+        response = self.client.get(f'/countries/{country.country_name}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'country_articles.html')
+    
+    def test_get_country_page(self):
+        response = self.client.get('/countries/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'countries.html')
+    
+    # def test_get_add_country_page(self):
+    #     pass
+    
+    # def test_get_edit_country_page(self):
+    #     pass
+
+    def test_get_article_detail_page(self):
+        user = User.objects.create(username='fake')
+        country = Country.objects.create(
+            country_name='Fake',
+            approve_country=True)
+        article = Article.objects.create(
+            title='Third Test Article',
+            slug='third-test-article',
+            country_id=1, author_id=1,
+            attraction='Third Test Article',
+            summary='Summary',
+            content='Content',
+            approved=True)
+        response = self.client.get(f'/{article.slug}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'article_detail.html')
+    
+    # def test_get_add_article_page(self):
+    #     pass
+    
+    # def test_get_edit_article_page(self):
+    #     pass
