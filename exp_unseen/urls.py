f8ac71722b0e4b1d3aadf62ff1118a164881ab8e
Ben Smith <bens6477@gmail.com>
2022-12-20 02:40:38 +0000

    Added custom 404 error page

--- exp_unseen/urls.py
+++ exp_unseen/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
 ]
+
+handler404 = 'exp_unseen.views.handler404'
